760eb57375255500a9447bea0790296099a182d9
yangjianwei <2017585616@qq.com>
2019-05-22 09:47:00 +0800

    add TCP/UDP的代码

---
+++ socket_protocol/src/main/java/com/inspur/App.java
@@ -0,0 +1,22 @@
+package com.inspur;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) throws InterruptedException {
+
+        for(int i = 0; i < 10000; i++){
+            for(int j = 0; j < 100; j++){
+                System.out.println("j------:" + j);
+                if(j == 10){
+                    break;
+                }
+            }
+            System.out.println("i--------:" + i);
+            Thread.currentThread().sleep(1000);
+        }
+
+    }
+}
